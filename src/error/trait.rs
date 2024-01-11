beec1d53463644e1b893edf1fe96c39bbdb11d4d
Alexi Peck <alexinicolaspeck@gmail.com>
2024-01-11 11:52:13 +1100

    Add another error wrapper variant which implements Clone

--- src/error/trait.rs
+++ src/error/trait.rs
@@ -11,3 +11,17 @@ macro_rules! impl_error_wrapper {
         }
     };
 }
+
+#[macro_export]
+macro_rules! impl_error_wrapper_clonable {
+    ($wrapper_type:ident, $inner_type:ty) => {
+        #[derive(thiserror::Error, Debug, Clone)]
+        pub struct $wrapper_type(#[from] pub $inner_type);
+
+        impl std::fmt::Display for $wrapper_type {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{}", self.0)
+            }
+        }
+    };
+}
