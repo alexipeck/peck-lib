9220c1f4a86c37cea78f1c17c7058cc24b81c407
Alexi Peck <alexinicolaspeck@gmail.com>
2024-01-14 16:21:46 +1100

    Add error wrapper to simplify error handing for projects depending on this

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -1,6 +1,16 @@
 pub mod r#trait;
 
+use thiserror::Error;
+
 use crate::impl_error_wrapper;
 
 impl_error_wrapper!(ReqwestError, reqwest::Error);
 impl_error_wrapper!(SerdeJsonError, serde_json::error::Error);
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("Reqwest({0})")]
+    Reqwest(ReqwestError),
+    #[error("SerdeJson({0})")]
+    SerdeJson(SerdeJsonError),
+}
