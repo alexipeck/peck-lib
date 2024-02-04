93c16c9f29b894f42ee22a295b40cf469b4e75d1
Alexi Peck <alexinicolaspeck@gmail.com>
2024-02-04 15:59:02 +1100

    Update UIDAuthority to use the tokio::sync::Mutex rather than parking_lot::Mutex

--- src/uid/authority.rs
+++ src/uid/authority.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashSet, sync::Arc};
 
-use parking_lot::Mutex;
+use tokio::sync::Mutex;
 use uuid::Uuid;
 
 use super::error::UIDAuthorityError;
@@ -16,27 +16,27 @@ impl From<Arc<Mutex<HashSet<Uuid>>>> for UIDAuthority {
 }
 
 impl UIDAuthority {
-    pub fn generate_uid(&self) -> Uuid {
+    pub async fn generate_uid(&self) -> Uuid {
         loop {
             let uid = Uuid::new_v4();
-            if !self.registry.lock().insert(uid) {
+            if !self.registry.lock().await.insert(uid) {
                 continue;
             }
             return uid;
         }
     }
 
-    pub fn insert(&self, uid: Uuid) -> Result<(), UIDAuthorityError> {
-        if !self.registry.lock().insert(uid) {
+    pub async fn insert(&self, uid: Uuid) -> Result<(), UIDAuthorityError> {
+        if !self.registry.lock().await.insert(uid) {
             return Err(UIDAuthorityError::DuplicateUIDInserted(uid));
         } else {
             Ok(())
         }
     }
 
-    pub fn insert_bulk(&self, uids: Vec<Uuid>) -> Result<(), UIDAuthorityError> {
+    pub async fn insert_bulk(&self, uids: Vec<Uuid>) -> Result<(), UIDAuthorityError> {
         let mut duplicates: Vec<Uuid> = Vec::new();
-        let mut lock = self.registry.lock();
+        let mut lock = self.registry.lock().await;
         for uid in uids {
             if !lock.insert(uid) {
                 duplicates.push(uid);
