e0f0fc52d56afd9769d940dfce380e215a1ed381
Alexi Peck <alexinicolaspeck@gmail.com>
2024-06-19 01:16:48 +0000

    Add function to parse a 6-digit slice from &str

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,3 +1,13 @@
 pub mod error;
 pub mod token_pair;
 pub mod user_login;
+
+pub fn str_to_two_fa(input: &str) -> Option<[u8; 6]> {
+    let bytes = input.as_bytes();
+    if bytes.len() != 6 {
+        return None;
+    }
+    let mut array = [0u8; 6];
+    array.copy_from_slice(bytes);
+    Some(array)
+}
