f864b73b63c452091c45285abab8a0700a41e3d4
Alexi Peck <alexinicolaspeck@gmail.com>
2024-06-19 00:54:58 +0000

    Add function to handle get request with params

--- src/http/request.rs
+++ src/http/request.rs
@@ -272,3 +272,74 @@ where
         )),
     };
 }
+
+pub async fn get_query_params<T, P>(
+    url: &str,
+    client: Client,
+    bearer_token: Option<&str>,
+    params: &P,
+) -> Result<T, Error>
+where
+    T: DeserializeOwned,
+    P: Serialize + ?Sized,
+{
+    get_query_params_debug(url, client, bearer_token, params, false).await
+}
+
+///debug_text_output refers specifically to when deserialization fails
+pub async fn get_query_params_debug<T, P>(
+    url: &str,
+    client: Client,
+    bearer_token: Option<&str>,
+    params: &P,
+    debug_text_output: bool,
+) -> Result<T, Error>
+where
+    T: DeserializeOwned,
+    P: Serialize + ?Sized,
+{
+    let mut request = client
+        .get(url)
+        .query(params)
+        .header("accept", "application/json");
+    if let Some(bearer_token) = bearer_token {
+        request = request.header("Authorization", format!("Bearer {}", bearer_token));
+    }
+
+    let response = match request.send().await {
+        Ok(response) => response,
+        Err(err) => {
+            warn!("{}", err);
+            return Err(Error::Send(ReqwestError(err)));
+        }
+    };
+    return match response.status() {
+        StatusCode::OK => {
+            let response_text: String = match response.text().await {
+                Ok(response_text) => response_text,
+                Err(err) => {
+                    warn!("{}", err);
+                    return Err(Error::ConvertResponseToText(ReqwestError(err)));
+                }
+            };
+            if debug_text_output {
+                debug!("{}", response_text);
+            }
+            let data: T = match serde_json::from_str::<T>(&response_text) {
+                Ok(data) => data,
+                Err(err) => {
+                    warn!("{}", err);
+                    return Err(Error::DeserializeTypeFromText(SerdeJsonError(err)));
+                }
+            };
+            Ok(data)
+        }
+        other => Err(Error::InvalidResponse(
+            other,
+            response
+                .text()
+                .await
+                .unwrap_or("Response was not text".into()),
+        )),
+    };
+}
